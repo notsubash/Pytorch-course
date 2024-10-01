b68ab4599998029eb94dba7cb86e943aa22d1f75
Subash Pandey <axlesubash111@gmail.com>
2024-10-01 16:40:21 +0545

    Softmax function and Cross Entropy Loss for Binary class and Multi-class problems

---
+++ 09_Softmax_and_Cross_Entropy.py
@@ -0,0 +1,112 @@
+#Softmax
+import torch
+import torch.nn as nn
+import numpy as np
+
+#
+#        -> 2.0              -> 0.65  
+# Linear -> 1.0  -> Softmax  -> 0.25   -> CrossEntropy(y, y_hat)
+#        -> 0.1              -> 0.1                   
+#
+#     scores(logits)      probabilities
+#                           sum = 1.0
+#
+
+# Softmax applies the exponential function to each element, and normalizes
+# by dividing by the sum of all these exponentials
+# -> squashes the output to be between 0 and 1 = probability
+# sum of all probabilities is 1
+
+
+def softmax(x):
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
+
+x = np.array([2.0, 1.0, 0.1])
+outputs = softmax(x)
+print('softmax numpy:', outputs)
+
+x = torch.tensor([2.0, 1.0, 0.1])
+outputs = torch.softmax(x, dim=0)
+print(outputs)
+
+
+### --> Cross-Entropy Loss
+# Cross-entropy loss, or log loss, measures the performance of a classification model 
+# whose output is a probability value between 0 and 1. 
+# -> loss increases as the predicted probability diverges from the actual label
+
+def cross_entropy(actual, predicted):
+    loss = -np.sum(actual * np.log(predicted))
+    return loss # / float(predicted.shape[0])
+
+# y must be one hot encoded
+# if class 0: [1 0 0]
+# if class 1: [0 1 0]
+# if class 2: [0 0 1]
+Y =np.array([1,0,0])
+
+#y_pred has probabilities
+y_pred_good = np.array([0.7, 0.2, 0.1])
+y_pred_bad = np.array([0.1, 0.3, 0.6])
+l1 = cross_entropy(Y, y_pred_good)
+l2 = cross_entropy(Y, y_pred_bad)
+print(f"Loss1 entropy: {l1:.4f}")
+print(f"Loss2 entropy: {l2:.4f}")
+
+
+#### Using PyTorch
+loss = nn.CrossEntropyLoss()
+
+# 3 samples
+Y = torch.tensor([2, 0, 1])
+# n_samples * n_classes = 3 * 3 = 9
+Y_pred_good = torch.tensor([[0.1, 1.0, 2.1], [2.0, 1.0, 0.1], [0.1, 3.0, 0.1]])
+Y_pred_bad = torch.tensor([[2.1, 1.0, 1.0], [0.1, 1.0, 2.1], [0.1, 3.0, 0.1]])
+
+l1 = loss(Y_pred_good, Y)
+l2 = loss(Y_pred_bad, Y)
+print(f"Loss1 entropy: {l1.item()}")
+print(f"Loss2 entropy: {l2.item()}")
+
+#Getting the actual predictions
+_, predictions1 = torch.max(Y_pred_good, 1)
+_, predictions2 = torch.max(Y_pred_bad, 1)
+print(predictions1)
+print(predictions2)
+
+##### --> Multi_Class problem
+class NeuralNet2(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNet2, self).__init__()
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.linear2 = nn.Linear(hidden_size, num_classes)
+
+    def forwards(self, x):
+        out = self.linear1(x)
+        out = self.relu(out)
+        out = self.linear2(out)
+
+        return out
+    
+model = NeuralNet2(input_size=28*28, hidden_size=5, num_classes=3)
+criterion = nn.CrossEntropyLoss() #Applies Softmax
+
+##### --> Binary Classification
+class NeuralNet1(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(NeuralNet1, self).__init__()
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.linear2 = nn.Linear(hidden_size, 1)
+
+    def forwards(self, x):
+        out = self.linear1(x)
+        out = self.relu(out)
+        out = self.linear2(out)
+        #sigmoid at end
+        y_pred = torch.sigmoid(out)
+        return y_pred
+    
+model = NeuralNet1(input_size=28*28, hidden_size=5)
+criterion = nn.BCELoss #Applies Binary Cross Entropy Loss
