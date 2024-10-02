ad1045d2afce161be13ed93ccf9c64cd76aea255
Subash Pandey <axlesubash111@gmail.com>
2024-10-02 12:49:24 +0545

    CNN from scratch

---
+++ 12_Convolutiona_Neural_Network.py
@@ -0,0 +1,143 @@
+"""
+Formula to compute the output size of a convolutional layer
+
+output_size = (W - F + 2P) / S + 1
+
+where,
+W = input size
+F = filter size
+P = padding
+S = stride
+
+so, for an example, input = 5*5, filter = 3*3, padding = 1, stride = 1
+output_size = (5 - 3 + 2*1) / 1 + 1 = 4 + 1 = 5
+output_size = 5 , so 5*5
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils
+import torch.utils.data
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+#Hyper-parameters
+num_epochs = 4
+batch_size = 4
+learning_rate = 0.001
+
+# dataset has PILImage images of range [0,1].
+# We transform them to Tensors of normalized range [-1,1]
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+)
+
+train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+#Implement CNN
+class ConvNet(nn.Module):
+    def __init__(self):
+        super(ConvNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2,2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16*5*5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self,x):
+        #layer1
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.pool(x)
+
+        #layer2
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.pool(x)
+
+        #flatten
+        x = x.view(-1, 16*5*5)
+
+        # Fully connected layer 1
+        x = self.fc1(x)
+        x = F.relu(x)
+
+        # Fully connected layer 2
+        x = self.fc2(x)
+        x = F.relu(x)
+
+        x = self.fc3(x)
+        return x
+
+
+model = ConvNet().to(device)
+
+#loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+#Training loop
+n_total_steps = len(train_loader)
+
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        # origin shape: [4, 3, 32, 32] = 4, 3, 1024
+        # input_layer: 3 input channels, 6 output channels, 5 kernel size
+        images = images.to(device)
+        labels = labels.to(device)
+
+        # Forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i+1) % 2000 == 0:
+            print(f'epoch {epoch+1} / {num_epochs}, step {i+1}/{n_total_steps}, loss = {loss.item():.4f}')
+
+
+print('Training finished')
+
+#Test the model
+# In test phase, we don't need to compute gradients (for memory efficiency)
+with torch.no_grad():
+    n_correct = 0
+    n_samples = 0
+    n_class_correct = [0 for i in range(10)]
+    n_class_samples = [0 for i in range(10)]
+
+    #max returns (value ,index)
+    _, predicted = torch.max(outputs, 1)
+    n_samples += labels.size(0)
+    n_correct += (predicted == labels).sum().item()
+
+    for i in range(batch_size):
+        label = labels[i]
+        pred = predicted[i]
+        if (label == pred):
+            n_class_correct[label] += 1
+        n_class_samples[label] += 1
+
+    acc = 100.0 * n_correct / n_samples
+    print(f'Accuracy of the network: {acc} %')
+
+    for i in range(10):
+        acc = 100.0 * n_class_correct[i] / n_class_samples[i]
+        print(f'Accuracy of {classes[i]}: {acc} %')
