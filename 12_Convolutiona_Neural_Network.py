4bacaefb43cf0e1ef1b07d20c385d0b9f34852aa
Subash Pandey <axlesubash111@gmail.com>
2024-10-02 13:55:36 +0545

    CNN from scratch

--- 12_Convolutiona_Neural_Network.py
+++ 12_Convolutiona_Neural_Network.py
@@ -139,5 +139,5 @@ with torch.no_grad():
     print(f'Accuracy of the network: {acc} %')
 
     for i in range(10):
-        acc = 100.0 * n_class_correct[i] / n_class_samples[i]
+        acc = 100 * n_class_correct[i] / n_class_samples[i]
         print(f'Accuracy of {classes[i]}: {acc} %')
